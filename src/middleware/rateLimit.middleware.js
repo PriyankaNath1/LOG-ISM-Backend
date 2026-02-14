b7c4ad908e481cdfddacd1e7814f5e320344c0a2
PriyankaNath1 <130272891+PriyankaNath1@users.noreply.github.com>
2026-02-15 02:43:04 +0530

    changing auth rate limit

--- src/middleware/rateLimit.middleware.js
+++ src/middleware/rateLimit.middleware.js
@@ -22,7 +22,7 @@ export const generalLimiter = rateLimit({
  */
 export const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // Limit each IP to 5 requests per windowMs
+  max: 15, // Limit each IP to 5 requests per windowMs
   message: "Too many login/register attempts, please try again later.",
   standardHeaders: true,
   legacyHeaders: false,
